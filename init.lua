66a9ffbabe2cb5516705e29769e041e68414cb7d
landinjm <landinjm@umich.edu>
2026-04-08 11:16:15 -0400

    remove some things

--- init.lua
+++ init.lua
@@ -601,11 +601,6 @@ require('lazy').setup({
             '--header-insertion=never',
           },
         },
-        cmake = {},
-        fortls = {},
-        -- gopls = {},
-        pyright = {},
-        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
