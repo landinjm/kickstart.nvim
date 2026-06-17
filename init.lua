35443899f64eaa1bbda3950df01176476d28799b
landinjm <landinjm@umich.edu>
2026-06-17 13:07:56 -0400

    remove some plugins causing markdown issues

--- init.lua
+++ init.lua
@@ -570,6 +570,8 @@ require('lazy').setup({
         python = { 'isort', 'black' },
         fortran = { 'fprettify' },
         cmake = { 'gersemi' },
+        html = { 'prettier' },
+        htmldjango = { 'prettier' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -773,9 +775,9 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
